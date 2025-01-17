ce684c898b5d1bfd1b8f1ea88dcf42287541d7db
John Hartsfield <john@unilink.local>
2025-01-16 23:38:26 -0500

    updates

--- internal/components/nav/nav.js
+++ internal/components/nav/nav.js
@@ -1,13 +1,18 @@
 {{ define "nav.js" }}
+var vis = false;
 function showDash() {
     let nav = document.getElementById("following");
-    if (nav.offsetHeight > 100) {
+    console.log(vis);
+    if (vis) {
+        vis = false;
         nav.classList.remove("heightAnimate");
+        nav.innerHTML = "";
     } else {
         nav.classList.add("heightAnimate");
         for (i=0;i<following.length;i++) {
             nav.innerHTML = nav.innerHTML + "<div class='ftag'>" +following[i] +'</div>';
         }
+        vis = true;
     }
 }
 {{end}}
